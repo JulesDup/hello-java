b6a0049581117348300b15ec51d0bd72c9c45e7b
Jules Dupuis <jules.dupuis51@gmail.com>
2020-02-25 17:40:01 +0100

    systout2

--- src/dev/HelloJava.java
+++ src/dev/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		System.out.println("bienvenu sur la branche hotfix");
+		System.out.println("en faite je suis le master");
 
 	}
 
